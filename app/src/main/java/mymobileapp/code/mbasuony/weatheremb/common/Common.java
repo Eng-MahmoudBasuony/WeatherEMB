ec0459b9fb4e9ecfe967916045187a6451077276
mahmoud <mbasuony83@gmail.com>
2019-03-10 20:58:47 +0200

    Create fragment display - get location - Build Retrofit request to openWeatherMap

--- app/src/main/java/mymobileapp/code/mbasuony/weatheremb/common/Common.java
+++ app/src/main/java/mymobileapp/code/mbasuony/weatheremb/common/Common.java
@@ -1,8 +1,12 @@
 package mymobileapp.code.mbasuony.weatheremb.common;
 
+import android.location.Location;
+
 public class Common
 {
 
     public static final String APP_ID="84ac124788bc2a57fc3335b3eac8e9d5";
+    public static Location current_location=null;
+
 
 }
