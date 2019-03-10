06d6c87f7b5a7c4db7c49e77ae098142be448ef5
mahmoud <mbasuony83@gmail.com>
2019-03-10 15:35:45 +0200

    model structure json

---
+++ app/src/main/java/mymobileapp/code/mbasuony/weatheremb/model/Clouds.java
@@ -0,0 +1,17 @@
+package mymobileapp.code.mbasuony.weatheremb.model;
+
+public class Clouds
+{
+    private int all ;
+
+    public Clouds() {
+    }
+
+    public int getAll() {
+        return all;
+    }
+
+    public void setAll(int all) {
+        this.all = all;
+    }
+}
